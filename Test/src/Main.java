e21f45706848e8fdb89b3c5ddc29dc09dfe3ab6a
scarabush_scarabush <snusikfrescus@gmail.com>
2024-02-09 17:40:34 +0300

    MyLinkedList

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println();
+        MyLinkedList list = new MyLinkedList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+        System.out.println(list.get(1));
+        list.remove(1);
+        System.out.println(list);
     }
 }
